ca4cbf5501efcb57def54c9410f756343f1db1c9
Anubis-LT <gregory.leterte@gmail.com>
2019-10-29 09:05:54 +0100

    Rename file class

---
+++ class/characters.py
@@ -0,0 +1,89 @@
+""""
+Game: McGyver maze
+Creator: Grégory Le Terte
+
+Class: class_characters.py
+
+
+"""
+
+from class_item import Inventory
+from random import randint
+from class_config_json import Config_Json
+
+# ============================
+#  Base class of all personas
+# ============================
+class GamePersona():
+    """
+    Base class for all personas (Chars, NPC...) in the game
+    """
+
+    def __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        parameters = Config_Json('./ressources/config.json')
+        config_file=Config_Json.filemap(parameters, "constants")
+        size_sprite = config_file.get("size_sprites")
+
+        self.case_x = self.pos_x * size_sprite
+        self.case_y = self.pos_y * size_sprite
+
+    def case_position(self, lvl):
+        """
+        Returns the position of the persona
+        """
+        return (self.case_x, self.case_y)
+
+
+# ===========================
+#         NPCS' class
+# ===========================
+class NPC(GamePersona):
+    """
+    Class to create NPCS.
+    """
+
+    def __init__(self, lvl, name):
+        self.name = name
+
+        super().__init__()
+        self.pos_x, self.pos_y = self.random_npc_position(lvl,name)
+
+    def random_npc_position(self, lvl,name):
+        parameters = Config_Json('./ressources/config.json')
+        config_file = Config_Json.filemap(parameters, "constants")
+        size_sprite = config_file.get("size_sprites")
+        nb_sprite = config_file.get("nb_sprites")
+
+        # load
+        structure = Config_Json.filemap(parameters, "structures")
+        floor = structure["floor"]
+        floor_caractere = floor["caractere"]
+
+        """
+        Returns a random position for each instance of NPC
+        """
+        while lvl.maze_map[self.pos_y][self.pos_x] != floor_caractere:
+            self.pos_x = randint(10, (nb_sprite - 1))
+            self.pos_y = randint(10, (nb_sprite - 1))
+        self.case_x = self.pos_x * size_sprite
+        self.case_y = self.pos_y * size_sprite
+
+        # set the case name as NPCS' initial
+        lvl.maze_map[self.pos_y][self.pos_x] = name["caractere"]
+        return self.case_x, self.case_y
+
+
+# ===========================
+#     Characters' class
+# ===========================
+class Character(GamePersona):
+    """
+    Class to create playable characters.
+    """
+
+    def __init__(self, lvl):
+        self.lvl = lvl
+        self.inventory = Inventory()
+        super().__init__()
---
+++ class/images.py
@@ -0,0 +1,63 @@
+"""
+Game: McGyver maze
+Creator: Grégory Le Terte
+
+Class: class_images.py
+
+
+"""
+from class_config_json import Config_Json
+
+class Images:
+    """
+    Load images from different sources.
+    """
+
+    def __init__(self,parameters,required_items):
+        """
+            Load images Wall and Floor from stucture config.json
+        """
+        structure = Config_Json.filemap(parameters,"structures")
+        self.wall = structure["wall"]
+        self.wall_img = self.wall["file_picture"]
+
+        self.floor = structure["floor"]
+        self.floor_img = self.load_image(self.floor["file_picture"])
+
+        """
+           Load images Mac Gyver and Guardian from characters config.json
+        """
+        characters = Config_Json.filemap(parameters,"characters")
+        self.mcgyver = characters["mac_gyver"]
+        self.mcgyver_img = self.load_image(self.mcgyver["file_picture"])
+
+        self.guardian = characters["guardian"]
+        self.guardian_img = self.load_image(self.guardian["file_picture"])
+
+        """
+           Load images Needle,tube,ether from object config.json
+        """
+        objects = Config_Json.filemap(parameters, "object")
+        self.needle = objects["needle"]
+        self.needle_img = self.load_image(self.needle["file_picture"])
+
+        self.tube = objects["tube"]
+        self.tube_img = self.load_image(self.tube["file_picture"])
+
+        self.ether = objects["ether"]
+        self.ether_img = self.load_image(self.ether["file_picture"])
+
+
+    def load_image(self, filename):
+
+        """
+               Calls pygame to load image for the given filename
+               and convert alpha zones in transparent
+               """
+        try:
+            with open(filename):
+                return filename
+        except FileNotFoundError as fnferr:
+            print("Image {} could not be opened. \
+                      Here is the original message: {}".format(filename, fnferr))
+            exit()
